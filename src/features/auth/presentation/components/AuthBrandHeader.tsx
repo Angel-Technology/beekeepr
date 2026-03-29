ff33a6cc8509a4ddab932bdb6b837dbbffcb5311
Sammy <wemsamuel@gmail.com>
2026-03-29 11:15:57 -0700

    update: app header

--- src/features/auth/presentation/components/AuthBrandHeader.tsx
+++ src/features/auth/presentation/components/AuthBrandHeader.tsx
@@ -1,6 +1,6 @@
 import { View } from 'react-native';
 
-import LineBuzz from '@src/assets/svg/LineBuzz';
+import AppHeaderLineBuzz from '@src/assets/svg/AppHeaderLineBuzz';
 import LogoBuzzkeepr from '@src/assets/svg/LogoBuzzkeepr';
 
 export const AuthBrandHeader = () => {
@@ -8,8 +8,8 @@ export const AuthBrandHeader = () => {
     <View className="-mx-lg flex items-center self-stretch">
       <LogoBuzzkeepr width={300} height={65.782} />
 
-      <View className="absolute -right-[0.242px] top-[18px] self-stretch">
-        <LineBuzz width={300.242} height={80.5} />
+      <View className="absolute -right-[0.242px] top-[38px] self-stretch">
+        <AppHeaderLineBuzz width={300.242} height={80.5} />
       </View>
     </View>
   );
